a1e52ce6e67a7d90d2a2cc50bf28f2fcb7594b05
ndrd79 <ndrd7980@gmail.com>
2024-12-18 10:52:48 -0300

    Corrige gerenciamento de sessão do Supabase

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -3,9 +3,19 @@ import type { Database } from '@/types/supabase'
 
 export const createClient = () => {
   return createClientComponentClient<Database>({
-    options: {
-      persistSession: true,
-      autoRefreshToken: true,
+    cookies: {
+      get(name: string) {
+        return document.cookie
+          .split('; ')
+          .find((row) => row.startsWith(`${name}=`))
+          ?.split('=')[1]
+      },
+      set(name: string, value: string, options: any) {
+        document.cookie = `${name}=${value}; path=/`
+      },
+      remove(name: string, options: any) {
+        document.cookie = `${name}=; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT`
+      },
     },
   })
 }
