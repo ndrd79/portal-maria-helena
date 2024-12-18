3e1116e636789dbce2f04428168e316825d509ae
ndrd79 <ndrd7980@gmail.com>
2024-12-18 10:55:00 -0300

    Simplifica cliente Supabase

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -2,20 +2,5 @@ import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
 import type { Database } from '@/types/supabase'
 
 export const createClient = () => {
-  return createClientComponentClient<Database>({
-    cookies: {
-      get(name: string) {
-        return document.cookie
-          .split('; ')
-          .find((row) => row.startsWith(`${name}=`))
-          ?.split('=')[1]
-      },
-      set(name: string, value: string, options: any) {
-        document.cookie = `${name}=${value}; path=/`
-      },
-      remove(name: string, options: any) {
-        document.cookie = `${name}=; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT`
-      },
-    },
-  })
+  return createClientComponentClient<Database>()
 }
